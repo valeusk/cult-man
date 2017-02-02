902c6e60d12dc9c5d24157799bd339a13919339b
valeusk <valeusk@gmail.com>
2017-02-02 17:34:18 +0300

    Убрал posted in

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -49,7 +49,7 @@ function cult_man_entry_footer() {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( esc_html__( ', ', 'cult-man' ) );
 		if ( $categories_list && cult_man_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'cult-man' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'cult-man' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 
 		/* translators: used between list items, there is a space after the comma */
